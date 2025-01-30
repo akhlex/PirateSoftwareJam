1b95a1e77d041a84db9871c8ffe257ccd5525d41
Akhil Alex <akhilalex003z@gmail.com>
2025-01-30 22:08:55 +0530

    added shitty title screen

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -5,9 +5,9 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
-func _on_options_pressed():
-	pass # Replace with function body.
-
-
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
